cf05fb4b7e6ac43baf8f8d8909d3aaae4b7a2b72
Arantesmateus <mateuscarantes@outlook.com>
2026-04-09 15:52:39 -0300

    Sending updated files with new aditions to perfil_investidor

--- src/app.py
+++ src/app.py
@@ -1,13 +1,30 @@
-from ctypes import alignment
 import json
 import pandas as pd
 import streamlit as st
 import requests
 from datetime import datetime
+from dotenv import load_dotenv
+import os
 
 # --------- SETTINGS ---------
-OLLAMA_URL = 'http://127.0.0.1:11434/api/generate'
-MODEL = 'phi4-mini'
+load_dotenv()
+OLLAMA_URL = os.getenv('OLLAMA_URL')
+MODEL = os.getenv('MODEL')
+
+# --------- Functions ----------
+def hora_atual():
+  return datetime.now().strftime("%H:%M")
+
+def load_file(file_path:str, wrap_style: bool=False):
+  with open(file_path) as f:
+    content = f.read()
+  if wrap_style:
+    content = f'<style>{content}</style>'
+  st.markdown(content, unsafe_allow_html=True)
+
+def load_promt(file_path:str):
+  with open(file_path) as f:
+    return f.read()
 
 
 # --------- Load knowleadge base ---------
@@ -34,23 +51,8 @@ context = f'''
 
 # --------- SYSTEM PROMPT ---------
 
-SYSTEM_PROMPT = ''' 
-Você é uma assistente virtual chamada Investa, você deve agir como humano então você tem um nome pessoal.
+SYSTEM_PROMPT = load_promt('./prompts/system_prompt.txt')
 
-REGRAS:
-- Nunca recomende investimentos específicos, apenas explique como funcionam;
-- Jamais responda a perguntas fora do tema finanças pessoais. Quando ocorrer, responda lembrando o seu papel de assistente financeira
-- Não responda sobre coisas fora do assunto de finanças pessoais
-- Use os dados fornecidos para dar exemplos personalizados;
-- Linguagem simples, como se explicasse para um amigo;
-- Se não souber algo, admita: "Não tenho essa informação, mas posso explicar...";
-- Sempre pergunte se o cliente entendeu;
-- Responda de forma sucinta e direta, com no máximo 3 parágrafos.
-- Sempre responda no feminino
-- Seja simpática e acolhedora
-- Certifique-se de que as palavras estão corretas antes de falar com o clientes
-- Sempre pular uma linha quando terminar de escrever um paragrafo
-'''
 
 # --------- CALL OLLAMA ---------
 
@@ -68,35 +70,20 @@ def perguntar(msg):
     r = requests.post(OLLAMA_URL, json={"model": MODEL, "prompt": prompt, "stream":False})
     return r.json()['response']
 
-def hora_atual():
-  return datetime.now().strftime("%H:%M")
-
-def load_file(file_path:str, wrap_style: bool=False):
-  with open(file_path) as f:
-    content = f.read()
-  if wrap_style:
-    content = f'<style>{content}</style>'
-  st.markdown(content, unsafe_allow_html=True)
 
 
 # ----------- INTERFACE ----------
-st.set_page_config(page_title="Investa. Sua assistente de investimentos", page_icon="💰", layout='centered')
+st.set_page_config(page_title="Investa", page_icon="None", layout='centered')
 
 # ---------- Custom CSS for the chat ----------
 load_file('./style.css', wrap_style = True)
 
-# --------- Custom title -------
-load_file('./html/header.html')
 
 # ---------- Initilize the history conversation ----------
 if 'messages' not in st.session_state:
     st.session_state['messages'] = []
-    welcome = "Olá! Sou a **Investa**, sua assistente financeira 💚 Como posso te ajudar hoje?"
-    st.session_state['messages'].append({
-        'role': 'assistant',
-        'content': welcome,
-        'time': hora_atual()
-    })
+    welcome = f"Olá {profile['nome']}! Sou a **Investa**, Como posso te ajudar hoje?"
+    st.title(welcome)
 
 # --------- Show consersation history --------
 for msg in st.session_state['messages']:
